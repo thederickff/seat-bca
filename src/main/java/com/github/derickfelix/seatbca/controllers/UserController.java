77eeb3918517f4fa944b1cdc86dba4517edbc227
Derick Florencio <derickfelix@MBP-de-Derick.lan>
2023-08-06 16:44:42 +0100

    feat: implement store method

--- src/main/java/com/github/derickfelix/seatbca/controllers/UserController.java
+++ src/main/java/com/github/derickfelix/seatbca/controllers/UserController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -45,5 +47,13 @@ public class UserController {
     }
 
     return null;
+  }
+
+  @PostMapping
+  public User store(@RequestBody User user) {
+    if (user.getName() == null) {
+      return null;
+    }
+    return this.repository.save(user);
   }
 }
